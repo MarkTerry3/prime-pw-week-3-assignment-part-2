d8f8a581784c8588317619da4f2e8124d0f3f5db
Mark Terry <mark.terry93@gmail.com>
2021-12-16 19:48:41 -0600

    completed some things

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -6,17 +6,24 @@ console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
+for (let i=0; i<4; i++) {
   console.log(i);
 }
 
-// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
+// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5
 //   - Which part of the example loop do you need to change to do this?
-console.log('count from 0 to 5');
+console.log('Count from 0 - 5');
+for (let i=0; i<6; i++){
+  console.log(i);
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
-console.log('count from 3 to 5');
+console.log('Count from 3 - 5');
+for (let i=3; i<6; i++){
+  console.log(i);
+}
+
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
@@ -26,7 +33,7 @@ console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 console.log('STRETCH: countdown from 5 to 0');
 
 
-// 2. For of loops  
+// 2. For of loops
 console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
@@ -34,7 +41,7 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 console.log('Some stars:');
 
 
-// 3. While loops  
+// 3. While loops
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
@@ -45,3 +52,31 @@ console.log('count from 0 to 5');
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+
+
+
+let index = 0;
+const max = 10;
+
+while (index < max) {
+  console.log('In while loop, index:', index);
+  index++;
+}
+
+for (let i = 0; i < max; i++) {
+  console.log('in for loop. i:', i);
+}
+
+
+let hand = ['Q', '3', 'J', '9', '6', 'K', 'A']
+
+for( let i=0; i<hand.length; i++ ){
+console.log('for loop,', hand[ i ]);
+if ( hand[ i ] === '9' ) {
+  console.log( 'Match!!!');{}
+}
+}
+
+for( i in hand ){
+  console.log("for in loop", hand[ i ]);
+}
